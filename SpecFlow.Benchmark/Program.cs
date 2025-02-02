75dfb2bbebc0cfe7cc4e5103c8fe341009f9fb40
virmaxdam <damien.oria@gmail.com>
2025-02-02 21:59:32 +0100

    Benchmark using Lazy

--- SpecFlow.Benchmark/Program.cs
+++ SpecFlow.Benchmark/Program.cs
@@ -1,23 +1,43 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Text;
 using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Diagnostics.dotMemory;
+using BenchmarkDotNet.Engines;
 using BenchmarkDotNet.Running;
 
-var result = BenchmarkRunner.Run<BenchmarkToExecute>();
+BenchmarkRunner.Run<BenchmarkToExecute>();
 
-[SimpleJob] // Mean / Error / StdDev
-[MemoryDiagnoser]
+
+//[SimpleJob] // Mean / Error / StdDev
+//[MemoryDiagnoser]
+
+// Enables dotMemory profiling for all jobs
+[DotMemoryDiagnoser]
+// Adds the default "external-process" job
+// Profiling is performed using dotMemory Command-Line Profiler
+// See: https://www.jetbrains.com/help/dotmemory/Working_with_dotMemory_Command-Line_Profiler.html
+[SimpleJob]
+// Adds an "in-process" job
+// Profiling is performed using dotMemory SelfApi
+// NuGet reference: https://www.nuget.org/packages/JetBrains.Profiler.SelfApi
+[InProcess]
 public class BenchmarkToExecute
 {
     //private string? fileContent;
 
     [Benchmark()]
-    public void CheckReadAllText()
+    public string CheckReadAllText()
     {
-        var test = Task.Run(() => "toto").Result;
+        Lazy<string> x = new Lazy<string>(() => new StringBuilder().ToString());
+        var s = x.Value;
+        return s;
     }
     [Benchmark()]
-    public void CheckReadAllText2()
+    public string CheckReadAllText2()
     {
-        //var test = "toto";
+        var x = new StringBuilder().ToString();
+        var s = x;
+        return s;
     }
 }
